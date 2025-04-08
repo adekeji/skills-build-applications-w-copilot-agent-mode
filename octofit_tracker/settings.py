b36f84c96f9a49a736aa33a6bf200fee584e6cc9
Michael Biodun Olomide <39876588+adekeji@users.noreply.github.com>
2025-04-08 07:23:45 +0000

    Commit changes

--- octofit_tracker/settings.py
+++ octofit_tracker/settings.py
@@ -75,9 +75,9 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "djongo",
-        "NAME": "octofit_db",
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'octofit_db',
     }
 }
 
