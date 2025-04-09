faf7d04721db4b43f3fd14fcaf1be98125ef6be4
Michael Biodun Olomide <39876588+adekeji@users.noreply.github.com>
2025-04-09 15:24:10 +0000

    Commit changes

--- octofit-tracker/frontend/src/App.js
+++ octofit-tracker/frontend/src/App.js
@@ -5,29 +5,37 @@ import Leaderboard from './components/Leaderboard';
 import Teams from './components/Teams';
 import Users from './components/Users';
 import Workouts from './components/Workouts';
+import './App.css';
+import logo from './octofitapp-small.png';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li><Link to="/activities">Activities</Link></li>
-            <li><Link to="/leaderboard">Leaderboard</Link></li>
-            <li><Link to="/teams">Teams</Link></li>
-            <li><Link to="/users">Users</Link></li>
-            <li><Link to="/workouts">Workouts</Link></li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/activities" element={<Activities />} />
-          <Route path="/leaderboard" element={<Leaderboard />} />
-          <Route path="/teams" element={<Teams />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/workouts" element={<Workouts />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="logo">
+        <img src={logo} alt="Octofit Logo" />
+        <h1>Welcome to Octofit</h1>
+      </header>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li><Link to="/activities">Activities</Link></li>
+              <li><Link to="/leaderboard">Leaderboard</Link></li>
+              <li><Link to="/teams">Teams</Link></li>
+              <li><Link to="/users">Users</Link></li>
+              <li><Link to="/workouts">Workouts</Link></li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/activities" element={<Activities />} />
+            <Route path="/leaderboard" element={<Leaderboard />} />
+            <Route path="/teams" element={<Teams />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/workouts" element={<Workouts />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
   );
 }
 
